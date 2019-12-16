df46406870dde981d3279568fa96a3d7467c220b
Patrick Breheny <patrick-breheny@uiowa.edu>
2019-12-16 13:26:45 -0600

    scrub-url.R to fix bib file for SMIMR submission

---
+++ sub/smimr/scrub-url.R
@@ -0,0 +1,6 @@
+bib <- readLines('../../articles.bib')
+bib <- bib[!grepl('eprint', bib)]
+url <- grepl("url\\s*=", bib)
+rproj <- grepl("R-project\\.org", bib)
+bib <- bib[!url | rproj]
+writeLines(bib, 'articles.bib')
