6e17421c81a1bf30f780498c0f264ad923edeea3
Patrick Breheny <patrick-breheny@uiowa.edu>
2022-10-27 13:24:21 -0500

    Edits to responses

--- sub/smimr/1/scrub-url.R
+++ sub/smimr/1/scrub-url.R
@@ -1,4 +1,4 @@
-bib <- readLines('../../articles.bib')
+bib <- readLines('../../../articles.bib')
 bib <- bib[!grepl('eprint', bib)]
 url <- grepl("url\\s*=", bib)
 rproj <- grepl("R-project\\.org", bib)
